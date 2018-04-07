25f262dfb673823ca75c36fe2d0dcfb761346f05
DaiDuong <daniel.do1992@gmail.com>
2018-04-07 23:24:22 +0700

    Redirects to index page if the requested page is not found

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :find_project, only: [:show, :update, :edit]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
   def index
     @projects = Project.all
   end
@@ -38,19 +38,23 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
 
     flash[:notice] = "Project has been deleted"
     redirect_to projects_path
   end
+
   private
 
   def project_params
     params.require(:project).permit(:name, :description)
   end
 
-  def find_project
+  def set_project
     @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found"
+    redirect_to projects_path
   end
+
 end
