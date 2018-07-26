b2da399600864121f2a8c9253bfe70fe4859d0f0
DaiDuongPixta <duong.dodai@pixta.co.jp>
2018-07-26 23:24:21 +0700

    Display a comment's state transition

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,7 +2,21 @@ class Role < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
-  def self.available_roles
-    %w(manager editor viewer)
+  AVAILABLE_ROLES = %w(manager editor viewer).freeze
+
+  scope :viewer, -> { where(role: "viewer") }
+  scope :editor, -> { where(role: "editor") }
+  scope :manager, -> { where(role: "manager") }
+
+  AVAILABLE_ROLES.each do |query_role|
+    define_method "#{query_role}?" do
+      role == query_role
+    end
+  end
+
+  class << self
+    def available_roles
+      AVAILABLE_ROLES
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  scope :excluding_archived, lambda { where(archived_at: nil)}
+  scope :excluding_archived, -> { where(archived_at: nil) }
 
   def to_s
     "#{email} (#{admin? ? "Admin" : "User"})"
