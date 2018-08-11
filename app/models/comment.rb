fdf7fc5e50d2275f3493861781ff9060efc2b5dd
DaiDuongPixta <duong.dodai@pixta.co.jp>
2018-08-11 17:18:58 +0700

    Automatically subscribe users to a ticket when they comment on it

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,6 +13,7 @@ class Comment < ActiveRecord::Base
   before_create :set_previous_state
   after_create :set_ticket_state
   after_create :associate_tags_with_ticket
+  after_create :author_watches_ticket
 
   private
 
@@ -33,4 +34,9 @@ class Comment < ActiveRecord::Base
     end
   end
 
+  def author_watches_ticket
+    if author.present? && !ticket.watchers.include?(author)
+      ticket.watchers << author
+    end
+  end
 end
